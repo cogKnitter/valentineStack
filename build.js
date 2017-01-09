df2b0a3c50d12794ed0166e5da73df974d077029
Mariah Tree <mariah@filestack.com>
2017-01-08 20:43:57 -0600

    updating package json

--- build.js
+++ build.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 import App from './modules/App'
-import Projects from './modules/Projects'
-
 
 
 render((
   <Router history={browserHistory}>
-    <Route path="/" component={App}>
-    </Route>
+    <Route path="/" component={App}/>
   </Router>
 ), document.getElementById('app'))

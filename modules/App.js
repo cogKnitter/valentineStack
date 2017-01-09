789161996a2bda0a0ce8f6254b6967c1e202a060
Mariah Tree <mariah@filestack.com>
2017-01-09 13:40:32 -0600

    added styles and put array placeholder in state to accept collage items

--- modules/App.js
+++ modules/App.js
@@ -6,8 +6,8 @@ import PickFiles from './PickFiles.js'
 export default React.createClass({
   render() {
     return (
-      <main>
-        <h1 className="list__heading">valentine collage</h1>
+      <main className="app__body">
+        <h1 className="app__heading">valentine collage</h1>
         <div>
           <PickFiles/>
         </div>
--- modules/PickFiles.js
+++ modules/PickFiles.js
@@ -4,15 +4,22 @@ import {Link} from 'react-router'
 
 
 export default React.createClass({
-
+getInitialState() {
+  return {
+  collageImages: []
+  }
+},
 uploadImg() {
   filepicker.pickMultiple(
     {
       mimetype: 'image/*',
-      maxFiles: 9
+      maxFiles: 9,
+      services: ['COMPUTER', 'IMAGE_SEARCH', 'URL', 'FACEBOOK', 'INSTAGRAM', 'PICASA', 'DROPBOX', 'BOX', 'CONVERT']
     },
     function(Blobs){
       console.log(JSON.stringify(Blobs));
+      let newBlobs= [];
+      
     },
     function(FPError){
       console.log(FPError.toString());
@@ -20,11 +27,15 @@ uploadImg() {
   },
   render () {
     return (
+      <section>
       <div>
             <button id="button-upload" type="button" className="btn btn-default filepicker" onClick={() => this.uploadImg()}>
             Upload Files
           </button>
       </div>
+      <div>
+      </div>
+      </section>
     )
   }
   })
